bcbb8afa16bb47c65526efefbb0dba068f6d2efd
abbyshrack <theotherblurryface@gmail.com>
2019-05-22 15:42:34 -0600

    Update application.py
    Add necessary base functions and index() route

--- application.py
+++ application.py
@@ -1,3 +1,18 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/a")
 def a():
     return render_template("a.html")
